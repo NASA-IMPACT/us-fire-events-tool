611328dc19f1f30bd8685feae6dd950762020af3
Gjore Milevski <dzole0311@gmail.com>
2025-04-30 22:16:20 +0200

    Increase picking radius

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -71,6 +71,7 @@ const MapView = () => {
             onLoad={() => {
                 setTimeout(collectVisibleFeatures, 500);
             }}
+            pickingRadius={20}
             useDevicePixels={false}
             getTooltip={({ object }) => {
                 if (!object || !object.properties) return null;
