4f9fb1bc04e6a7dcebfcf01505fe63c1e7a2177b
Gjore Milevski <dzole0311@gmail.com>
2025-07-22 12:35:46 +0200

    Squashed commit of the following:
    commit 6e0fa23f67ce6e62d0d3f4dded2efeb7fc161def
    Author: Gjore Milevski <dzole0311@gmail.com>
    Date:   Tue Jul 22 09:30:39 2025 +0200
    
        fix: Add a settled view state prop that's updated only when the interaction stops, clean up, bump version

--- src/state/slices/mapSlice.ts
+++ src/state/slices/mapSlice.ts
@@ -1,37 +1,45 @@
 import { StateCreator } from 'zustand';
 import { ViewState } from 'react-map-gl/mapbox';
+import { INITIAL_VIEW_STATE } from '@/components/layers';
 
 export interface MapState {
+  // Real-time view state that updates continuously during map interactions
   viewState: ViewState;
+  // Debounced view state used for URL synchronization, only updates after interactions complete.
+  // This prevents browser history pollution and URL thrashing during dragging/zooming
+  // Use this state for syncing with URL params
+  viewStateForUrl: ViewState;
   mapBounds: [number, number, number, number] | null;
   layerOpacity: number;
 }
 
 export interface MapActions {
-  setViewState: (viewState: ViewState) => void;
+  setViewState: (viewState: ViewState, transitionOptions?: any) => void;
+  setViewStateForUrl: (viewState: ViewState) => void;
   setMapBounds: (bounds: [number, number, number, number] | null) => void;
   setLayerOpacity: (opacity: number) => void;
+  // Updates the settled view state for URL syncing in a debounced way after all user interactions complete
+  updateViewStateForUrl: (updates: Partial<ViewState>) => void;
 }
 
 export type MapSlice = MapState & MapActions;
 
 export const createMapSlice: StateCreator<MapSlice> = (set) => ({
-  viewState: {
-    longitude: -95.7129,
-    latitude: 37.0902,
-    zoom: 3,
-    pitch: 0,
-    bearing: 0,
-    padding: {
-      top: undefined,
-      bottom: undefined,
-      left: undefined,
-      right: undefined,
-    },
-  },
+  viewState: INITIAL_VIEW_STATE,
+  viewStateForUrl: INITIAL_VIEW_STATE,
   mapBounds: null,
   layerOpacity: 100,
-  setViewState: (viewState) => set({ viewState }),
+  setViewState: (viewState, transitionOptions) =>
+    set({
+      viewState: transitionOptions
+        ? { ...viewState, ...transitionOptions }
+        : viewState,
+    }),
+  setViewStateForUrl: (viewState) => set({ viewStateForUrl: viewState }),
   setMapBounds: (bounds) => set({ mapBounds: bounds }),
   setLayerOpacity: (opacity) => set({ layerOpacity: opacity }),
+  updateViewStateForUrl: (updates) =>
+    set((state) => ({
+      viewStateForUrl: { ...state.viewStateForUrl, ...updates },
+    })),
 });
