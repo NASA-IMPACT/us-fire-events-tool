760dba6b83c3ae81b5060887b191e38a387a2dff
Gjore Milevski <dzole0311@gmail.com>
2025-07-28 12:16:39 +0200

    feat: Add pagination when selecting events

--- src/state/slices/eventsSlice.ts
+++ src/state/slices/eventsSlice.ts
@@ -4,6 +4,7 @@ import {
   fetchFirePerimeters,
   fetchAlternativeFirePerimeters,
   fitMapToBounds,
+  fetchLayerPaginated,
 } from '../../utils/fireUtils';
 
 export interface EventsState {
@@ -44,10 +45,13 @@ export const createEventsSlice: StateCreator<
       set({ firePerimetersLoading: true });
 
       try {
-        let perimeters = await fetchFirePerimeters(
+        const perimetersBlob = await fetchLayerPaginated(
+          `${featuresApiEndpoint}/collections`,
+          'public.eis_fire_lf_perimeter_nrt',
           eventId,
-          featuresApiEndpoint
+          'geojson'
         );
+        let perimeters = await perimetersBlob.text().then(JSON.parse);
 
         if (!perimeters?.features?.length) {
           perimeters = await fetchAlternativeFirePerimeters(
@@ -62,7 +66,12 @@ export const createEventsSlice: StateCreator<
         });
 
         if (perimeters?.features?.length > 0) {
-          fitMapToBounds(perimeters.features[perimeters.features.length - 1]);
+          const sorted = [...perimeters.features].sort(
+            (a, b) =>
+              new Date(b.properties.t).getTime() -
+              new Date(a.properties.t).getTime()
+          );
+          fitMapToBounds(sorted[0]);
         }
       } catch (error) {
         console.error('Error fetching fire perimeters:', error);
