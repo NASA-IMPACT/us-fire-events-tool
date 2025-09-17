02ff8713422582597322ab7fda3a6fe3a1d9c1c1
Gjore Milevski <dzole0311@gmail.com>
2025-09-17 22:42:45 +0200

    fix: update endpoint

--- src/components/layers/GridLayer.tsx
+++ src/components/layers/GridLayer.tsx
@@ -34,7 +34,9 @@ export const createGridLayer = async ({ timeMarker, opacity = 92 }) => {
       .toString()
       .padStart(2, '0');
 
-    const imageUrl = `https://titiler.xyz/cog/preview.png?rescale=-127,128&url=vrt:///vsicurl/https://noaa-hrrr-bdp-pds.s3.amazonaws.com/hrrr.${runDateStr}/conus/hrrr.t${runHourStr}z.wrfsfcf${forecastHour}.grib2?bands=10,11&format=png`;
+    const imageUrl = `${
+      import.meta.env.VITE_OPENVEDA_API_ENDPOINT
+    }/raster/cog/preview.png?rescale=-127,128&url=vrt:///vsicurl/https://noaa-hrrr-bdp-pds.s3.amazonaws.com/hrrr.${runDateStr}/conus/hrrr.t${runHourStr}z.wrfsfcf${forecastHour}.grib2?bands=10,11&format=png`;
 
     let image;
 
