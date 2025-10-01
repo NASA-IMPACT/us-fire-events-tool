1d9a7af1f19af4065fcde359596d0a7852ef3d9f
sandrahoang686 <sandrahoang686@gmail.com>
2025-10-01 11:27:48 -0400

    Add SiteAlert Banner

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ import {
   ModalRef,
   ModalToggleButton,
   Tag,
+  SiteAlert
 } from '@trussworks/react-uswds';
 
 import './header.scss';
@@ -46,7 +47,9 @@ const Header: React.FC = () => {
   });
 
   return (
-    <header className="header bg-white height-5 padding-y-05 padding-x-105 display-flex flex-row  tablet:flex-justify-between flex-align-center width-full position-fixed top-0 left-0 z-top">
+    <>
+      <SiteAlert variant="emergency">Due to the lapse in federal government funding, NASA is not updating this website. We sincerely regret this inconvenience.</SiteAlert>
+    <header className="header bg-white height-5 padding-y-05 padding-x-105 display-flex flex-row  tablet:flex-justify-between flex-align-center width-full top-0 left-0 z-top">
       <div className="display-flex flex-align-center width-full tablet:flex-justify-start flex-justify">
         <a
           href="https://earthdata.nasa.gov/dashboard"
@@ -267,6 +270,7 @@ const Header: React.FC = () => {
         </div>
       </Modal>
     </header>
+    </>
   );
 };
 
