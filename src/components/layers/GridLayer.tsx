86043ebd28b36049de753f7518a127a52c9dca01
Henry Rodman <henry.rodman@gmail.com>
2026-07-29 07:24:04 -0500

    fix: remove /vsicurl prefix from HRRR URIs, encode to be safe (#51)
    resolves #48

--- src/components/layers/GridLayer.tsx
+++ src/components/layers/GridLayer.tsx
@@ -34,7 +34,8 @@ export const createGridLayer = async ({ timeMarker, opacity = 92 }) => {
       .toString()
       .padStart(2, '0');
 
-    const imageUrl = `https://openveda.cloud/api/raster/cog/preview.png?rescale=-127,128&url=vrt:///vsicurl/https://noaa-hrrr-bdp-pds.s3.amazonaws.com/hrrr.${runDateStr}/conus/hrrr.t${runHourStr}z.wrfsfcf${forecastHour}.grib2?bands=10,11&format=png`;
+    const vrtUrl = `vrt://https://noaa-hrrr-bdp-pds.s3.amazonaws.com/hrrr.${runDateStr}/conus/hrrr.t${runHourStr}z.wrfsfcf${forecastHour}.grib2?bands=10,11`;
+    const imageUrl = `https://openveda.cloud/api/raster/cog/preview.png?rescale=-127,128&url=${encodeURIComponent(vrtUrl)}&format=png`;
 
     let image;
 
